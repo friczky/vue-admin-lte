4e0055cb88597d50b57ed87b8ae6bdf9b7318321
Fadilah Riczky <friczky@gmail.com>
2022-08-04 09:18:01 +0700

    :zap: update plugins

---
+++ src/pages/HomeFe.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="home">
+        <div class="wrapper">
+
+            <!-- Preloader -->
+            <div class="preloader flex-column justify-content-center align-items-center">
+                <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+            </div>
+            <NavbarFe/>
+            <SidebarFe/>
+            <FooterFe/>
+        </div>
+    </div>
+
+</template>
+
+<script>
+
+import NavbarFe from '@/components/NavbarFe.vue'
+import SidebarFe from '@/components/SidebarFe.vue'
+import FooterFe from '@/components/FooterFe.vue'
+
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+export default {
+    name : 'HomeFe',
+
+    components: {
+        NavbarFe,
+        SidebarFe,
+        FooterFe
+    }
+}
+</script>
+
+<style>
+
+
+</style>
--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,16 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  chainWebpack: config => {
+    config
+      .plugin('provide')
+      .use(webpack.ProvidePlugin, [{
+        $: 'jquery',
+        jquery: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery'
+      }]);
+  },
+}
